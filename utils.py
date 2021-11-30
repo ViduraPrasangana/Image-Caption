c03b69763a9b90b6b84efe1a64cf183a2955535f
Vidura Prasangana <vidura.prasangana16@gmail.com>
2021-11-30 23:00:06 +0530

    err

--- utils.py
+++ utils.py
@@ -128,7 +128,7 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
                     # gray-scale
                     img = img[:, :, np.newaxis]
                     img = np.concatenate([img, img, img], axis=2)  # [256, 256, 1+1+1]
-                img = np.array(Image.fromarray(img).resize((256, 256)))
+                img = np.array(Image.fromarray(img.astype(np.uint8)).resize((256, 256)))
                 # img = imresize(img, (256, 256))
                 img = img.transpose(2, 0, 1)
                 assert img.shape == (3, 256, 256)
