ef8f90d48c34cbc03a8a537458d35153756268a7
Vidura Prasangana <vidura.prasangana16@gmail.com>
2021-12-05 04:36:29 +0530

    iu dataset

--- create_input_files.py
+++ create_input_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from utils import create_input_files
+from utils import create_input_files, create_input_files_2
 import argparse
 import os
 
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', default="coco", help='Default MSCOCO 14 Dataset.')
     parser.add_argument('--karpathy_json_path', default="./dataset/caption_dataset/dataset_coco.json",
                         help='path of captions dataset.')
-    parser.add_argument('--image_folder', default="./dataset", help='path of image dataset.')
-    parser.add_argument('--captions_per_image', type=int, default=5, help='How many captions each image has?')
+    parser.add_argument('--image_folder', default="./dataset/iu/image/images_normalized", help='path of image dataset.')
+    parser.add_argument('--captions_per_image', type=int, default=1, help='How many captions each image has?')
     parser.add_argument('--min_word_freq', type=int, default=5, help='the minimum frequency of words')
     parser.add_argument('--output_folder', default='./dataset/generated_data', help='output filepath.')
     parser.add_argument('--max_len', type=int, default=50, help='the maximum length of each caption.')
@@ -19,8 +19,7 @@ if __name__ == '__main__':
     if not (os.path.exists(args.output_folder) and os.path.isdir(args.output_folder)):
         os.makedirs(args.output_folder)
     # Create input files (along with word map)
-    create_input_files(dataset=args.dataset,
-                       karpathy_json_path=args.karpathy_json_path,
+    create_input_files_2(dataset=args.dataset,
                        image_folder=args.image_folder,
                        captions_per_image=args.captions_per_image,
                        min_word_freq=args.min_word_freq,
--- utils.py
+++ utils.py
@@ -14,6 +14,8 @@ from eval_func.bleu.bleu import Bleu
 from eval_func.rouge.rouge import Rouge
 from eval_func.cider.cider import Cider
 from eval_func.meteor.meteor import Meteor
+
+from tokenization import BertTokenizer
 # from eval_func.spice.spice import Spice
 
 
@@ -171,6 +173,176 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
 
             with open(os.path.join(output_folder, split + '_CAPLENS_' + base_filename + '.json'), 'w') as j:
                 json.dump(caplens, j)
+
+
+def create_input_files_2 (dataset, image_folder, captions_per_image, min_word_freq, output_folder,
+                       max_len=100):
+    """
+    Creates input files for training, validation, and test data.
+
+    :param dataset: name of dataset, one of 'coco', 'flickr8k', 'flickr30k'
+    :param karpathy_json_path: path of Karpathy JSON file with splits and captions
+    :param image_folder: folder with downloaded images
+    :param captions_per_image: number of captions to sample per image
+    :param min_word_freq: words occuring less frequently than this threshold are binned as <unk>s
+    :param output_folder: folder to save files
+    :param max_len: don't sample captions longer than this length
+    """
+
+    assert dataset in {'iu'}
+
+    # Read Karpathy JSON
+    with open("dataset/iu/train_data.json", 'r') as j:
+        train_data = json.load(j)
+    with open("dataset/iu/test_data.json", 'r') as j:
+        test_data = json.load(j)
+    
+    tokenizer = BertTokenizer.from_pretrained(
+            "bert-iu-xray",
+            do_lower_case=True
+        )
+
+    # Read image paths and captions for each image
+    train_image_paths = []
+    train_image_captions = []
+    val_image_paths = []
+    val_image_captions = []
+    test_image_paths = []
+    test_image_captions = []
+    word_freq = Counter()
+
+    for img in train_data:
+        captions = []
+        caption = img['findings']
+        if(caption == None):
+            continue
+            # Update word frequency
+        caption = tokenizer.tokenize(caption)
+        word_freq.update(caption)
+        if len(caption) <= max_len:
+            captions.append(caption)  # [[0], [1], [2], [3], [4]]
+
+        if len(captions) == 0:
+            continue
+
+        path = os.path.join(image_folder, img['filename']) 
+        
+        train_image_paths.append(path)
+        train_image_captions.append(captions)
+
+
+    for img in test_data:
+        captions = []
+        caption = img['findings']
+        if(caption == None):
+            continue
+            # Update word frequency
+        caption = tokenizer.tokenize(caption)
+        word_freq.update(caption)
+        if len(caption) <= max_len:
+            captions.append(caption)  # [[0], [1], [2], [3], [4]]
+
+        if len(captions) == 0:
+            continue
+
+        path = os.path.join(image_folder, img['filename']) 
+        
+        val_image_paths.append(path)
+        val_image_captions.append(captions)
+        
+        test_image_paths.append(path)
+        test_image_captions.append(captions)
+
+    # Sanity check
+    assert len(train_image_paths) == len(train_image_captions)
+    assert len(val_image_paths) == len(val_image_captions)
+    assert len(test_image_paths) == len(test_image_captions)
+    print("find {} training data, {} val data, {} test data".format(len(train_image_paths), len(val_image_paths), len(test_image_paths)))
+
+    # Create word map
+    words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
+    word_map = {k: v + 1 for v, k in enumerate(words)}  # word2id
+    word_map['<unk>'] = len(word_map) + 1
+    word_map['<start>'] = len(word_map) + 1
+    word_map['<end>'] = len(word_map) + 1
+    word_map['<pad>'] = 0
+
+    # Create a base/root name for all output files
+    base_filename = dataset + '_' + str(captions_per_image) + '_cap_per_img_' + str(min_word_freq) + '_min_word_freq'
+
+    # Save word map to a JSON
+    with open(os.path.join(output_folder, 'WORDMAP_' + base_filename + '.json'), 'w') as j:
+        json.dump(word_map, j)
+    print("{} words write into WORDMAP".format(len(word_map)))
+
+    # Sample captions for each image, save images to HDF5 file, and captions and their lengths to JSON files
+    seed(123)
+    for impaths, imcaps, split in [(train_image_paths, train_image_captions, 'TRAIN'),
+                                   (val_image_paths, val_image_captions, 'VAL'),
+                                   (test_image_paths, test_image_captions, 'TEST')]:
+
+        with h5py.File(os.path.join(output_folder, split + '_IMAGES_' + base_filename + '.hdf5'), 'a') as h:
+            # Make a note of the number of captions we are sampling per image
+            h.attrs['captions_per_image'] = captions_per_image
+
+            # Create dataset inside HDF5 file to store images
+            images = h.create_dataset('images', (len(impaths), 3, 256, 256), dtype='uint8')
+
+            print("\nReading %s images and captions, storing to file...\n" % split)
+
+            enc_captions = []
+            caplens = []
+
+            for i, path in enumerate(tqdm(impaths)):
+
+                # Sample captions
+                if len(imcaps[i]) < captions_per_image:
+                    captions = imcaps[i] + [choice(imcaps[i]) for _ in range(captions_per_image - len(imcaps[i]))]
+                else:
+                    captions = sample(imcaps[i], k=captions_per_image)
+
+                # Sanity check
+                assert len(captions) == captions_per_image
+
+                # Read images
+                img = imageio.imread(impaths[i])
+                # img = imread(impaths[i])
+
+                if len(img.shape) == 2:
+                    # gray-scale
+                    img = img[:, :, np.newaxis]
+                    img = np.concatenate([img, img, img], axis=2)  # [256, 256, 1+1+1]
+
+                img = np.array(Image.fromarray(img).resize((256, 256)))
+                # img = imresize(img, (256, 256))
+                img = img.transpose(2, 0, 1)
+                assert img.shape == (3, 256, 256)
+                assert np.max(img) <= 255
+
+                # Save image to HDF5 file
+                images[i] = img
+
+                for j, c in enumerate(captions):
+                    # Encode captions
+                    enc_c = [word_map['<start>']] + [word_map.get(word, word_map['<unk>']) for word in c] + [
+                        word_map['<end>']] + [word_map['<pad>']] * (max_len - len(c))
+
+                    # Find caption lengths
+                    c_len = len(c) + 2
+
+                    enc_captions.append(enc_c)
+                    caplens.append(c_len)
+
+            # Sanity check
+            assert images.shape[0] * captions_per_image == len(enc_captions) == len(caplens)
+
+            # Save encoded captions and their lengths to JSON files
+            with open(os.path.join(output_folder, split + '_CAPTIONS_' + base_filename + '.json'), 'w') as j:
+                json.dump(enc_captions, j)
+
+            with open(os.path.join(output_folder, split + '_CAPLENS_' + base_filename + '.json'), 'w') as j:
+                json.dump(caplens, j)
+
 
 
 def init_embedding(embeddings):
